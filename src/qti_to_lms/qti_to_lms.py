088d9d4029c7ee3afd2903bfed0f62ad1ff4c28c
Atefeh Nirumand <atefehnirumand@gmail.com>
2026-01-21 09:53:34 +0100

    Update qti_to_lms.py
    Refine QTI to LMS pipeline based on modifications in the QTI metamodel file.

--- src/qti_to_lms/qti_to_lms.py
+++ src/qti_to_lms/qti_to_lms.py
@@ -33,7 +33,7 @@ def fix_utf8(file_path):
 def run_qti_moodle_pipeline(xml_path: str):
     """Run the QTI to Moodle conversion pipeline."""
     # Parse QTI XML
-    besser_model: TestDefinition = qti_to_besser(xml_path=xml_path)
+    besser_model: AssessmentDefinition = qti_to_besser(xml_path=xml_path)
     if besser_model is None:
         print("Failed to parse QTI model. Nothing was generated.")
     else:
@@ -43,7 +43,7 @@ def run_qti_moodle_pipeline(xml_path: str):
     if besser_model:
         moodle_generator = MoodleGenerator(
             output_file_name="moodle.xml",
-            test_def=besser_model
+            assessment_def=besser_model
         )
         moodle_file_path = moodle_generator.generate()
         if moodle_file_path:
